091de9b6ed47beb0ba79a20a3708cddafdfaf359
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-04-23 22:37:26 +0700

    Finished JWT, protected route, register and login

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -29,7 +31,9 @@ router.post(
       // see if user exists
       let user = await User.findOne({ email });
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
       // Get users gravatar
@@ -51,10 +55,21 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-
       // Return jsonwebtoken
-
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSerect'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
