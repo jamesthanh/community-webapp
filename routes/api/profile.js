659bc8724ee83931dcc6194dd876e23f35ee447b
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-04-25 17:33:50 +0700

    fixed the update and delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -79,23 +79,23 @@ router.post(
     if (instagram) profileFileds.social.instagram = instagram;
     if (linkedin) profileFileds.social.linkedin = linkedin;
 
+    profileFileds;
+
     try {
       let profile = await Profile.findOne({ user: req.user.id });
+
       if (profile) {
         // Update
         profile = await Profile.findOneAndUpdate(
           { user: req.user.id },
           { $set: profileFileds },
-          {
-            new: true,
-          }
+          { new: true }
         );
         return res.json(profile);
       }
-
       // Create
       profile = new Profile(profileFileds);
-      await Profile.save();
+      await profile.save();
       res.json(profile);
     } catch (err) {
       console.error(err.message);
@@ -103,5 +103,58 @@ router.post(
     }
   }
 );
+
+// @route GET api/profile
+// @desc get all profiles
+// @access Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    DELETE api/profile
+// @desc     Delete profile, user & posts
+// @access   Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // // Remove user posts
+    // await Post.deleteMany({ user: req.user.id });
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
